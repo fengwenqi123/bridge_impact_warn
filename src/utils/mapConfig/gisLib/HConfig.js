224586773e520b6f3f8a9c20bb425e50f2c81928
zj <chunbaook@163.com>
2020-12-03 14:48:18 +0800

    生产环境地图部署

--- src/utils/mapConfig/gisLib/HConfig.js
+++ src/utils/mapConfig/gisLib/HConfig.js
@@ -4,18 +4,16 @@
  */
 
 /** ***********************************测试***************************************/
-export const GIS_HOST = 'http://192.168.1.209:8090' // 桥梁防撞底图地址
-export const GIS_SERVERIP = 'http://192.168.1.120:8001' // 通航要素接口(桥梁防撞只保留视频)
-export const GIS_SHIPWMS = 'https://map.cjbe88.com/geoserver/geobasedata/wms'
-export const GIS_SHIPWFS = 'https://map.cjbe88.com/geoserver/geobasedata/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geobasedata:zjghship&maxFeatures=20000&outputFormat=application/json'
+// export const GIS_HOST = 'http://192.168.1.209:8090' // 桥梁防撞底图地址
+// export const GIS_SERVERIP = 'http://192.168.1.120:8001' // 通航要素接口(桥梁防撞只保留视频)
+// export const GIS_SHIPWMS = 'https://map.cjbe88.com/geoserver/geobasedata/wms'
+// export const GIS_SHIPWFS = 'https://map.cjbe88.com/geoserver/geobasedata/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geobasedata:zjghship&maxFeatures=20000&outputFormat=application/json'
 /*******************************************************************************/
 
-// export const GIS_HOST = 'http://10.100.70.227:8080' // 桥梁防撞底图地址
-// export const GIS_SERVERIP = 'http://10.100.70.226/api' // 通航要素接口(桥梁防撞只保留视频)
-
-// 智慧海事船舶图层
-// export const GIS_SHIPWMS = 'http://10.100.70.227:8090/geoserver/geobase/wms'
-// export const GIS_SHIPWFS = 'http://10.100.70.227:8090/geoserver/geobase/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geobase:zjghship&maxFeatures=20000&outputFormat=application/json'
+export const GIS_HOST = 'http://172.20.20.15:8080' // 桥梁防撞底图地址
+export const GIS_SERVERIP = 'http://172.20.20.15/api' // 通航要素接口(桥梁防撞只保留视频)
+export const GIS_SHIPWMS = 'http://172.20.20.15:8080/geoserver/geobase/wms'
+export const GIS_SHIPWFS = 'http://172.20.20.15:8080/geoserver/geobase/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geobase:zjghship&maxFeatures=20000&outputFormat=application/json'
 
 // 船舶类型
 export const GIS_HXTSHIP = '浙海巡'

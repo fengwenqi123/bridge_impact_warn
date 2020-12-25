72555cee5133b7dc720e699300c18ebc01c386ba
zj <chunbaook@163.com>
2020-12-25 09:34:42 +0800

    1225 生产

--- src/utils/mapConfig/gisLib/HConfig.js
+++ src/utils/mapConfig/gisLib/HConfig.js
@@ -4,16 +4,16 @@
  */
 
 /** ***********************************测试***************************************/
-export const GIS_HOST = 'http://192.168.1.120:83/gis/' // 桥梁防撞底图地址
-export const GIS_SERVERIP = 'http://192.168.1.120:8001' // 通航要素接口(桥梁防撞只保留视频)
-export const GIS_SHIPWMS = 'https://map.cjbe88.com/geoserver/geobasedata/wms'
-export const GIS_SHIPWFS = 'https://map.cjbe88.com/geoserver/geobasedata/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geobasedata:zjghship&maxFeatures=20000&outputFormat=application/json'
+// export const GIS_HOST = 'http://192.168.1.120:83/gis/' // 桥梁防撞底图地址
+// export const GIS_SERVERIP = 'http://192.168.1.120:8001' // 通航要素接口(桥梁防撞只保留视频)
+// export const GIS_SHIPWMS = 'https://map.cjbe88.com/geoserver/geobasedata/wms'
+// export const GIS_SHIPWFS = 'https://map.cjbe88.com/geoserver/geobasedata/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geobasedata:zjghship&maxFeatures=20000&outputFormat=application/json'
 /*******************************************************************************/
 
-// export const GIS_HOST = '/gis' // 桥梁防撞底图地址
-// export const GIS_SERVERIP = 'http://172.20.20.15/api' // 通航要素接口(桥梁防撞只保留视频)
-// export const GIS_SHIPWMS = '/gis/geoserver/geobase/wms'
-// export const GIS_SHIPWFS = '/gis/geoserver/geobase/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geobase:ship_position&maxFeatures=20000&outputFormat=application/json'
+export const GIS_HOST = '/gis' // 桥梁防撞底图地址
+export const GIS_SERVERIP = 'http://172.20.20.15/api' // 通航要素接口(桥梁防撞只保留视频)
+export const GIS_SHIPWMS = '/gis/geoserver/geobase/wms'
+export const GIS_SHIPWFS = '/gis/geoserver/geobase/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geobase:ship_position&maxFeatures=20000&outputFormat=application/json'
 
 // 船舶类型
 export const GIS_HXTSHIP = '浙海巡'

602b88476ef44405bd8746c81e51e50e10f6a3a2
pengziyun <35458649+pengziyun@users.noreply.github.com>
2020-12-29 16:34:40 +0800

    12291

--- src/router/index.js
+++ src/router/index.js
@@ -1,43 +1,40 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import base from './modules/base'
-import system from './modules/system'
-import video from './modules/video'
-import warning from './modules/warning'
-import journal from './modules/journal'
-import yawWarning from './modules/yawWarning'
-import electronicFence from './modules/electronicFence'
-import map from './modules/map'
+import Vue from "vue";
+import Router from "vue-router";
+import base from "./modules/base";
+import system from "./modules/system";
+import video from "./modules/video";
+import warning from "./modules/warning";
+import journal from "./modules/journal";
+import yawWarning from "./modules/yawWarning";
+import electronicFence from "./modules/electronicFence";
+import map from "./modules/map";
 
-Vue.use(Router)
+Vue.use(Router);
 
 // 解决router回调报错问题
-const originalPush = Router.prototype.push
-Router.prototype.push = function push (location) {
-  return originalPush.call(this, location).catch(err => err)
-}
+const originalPush = Router.prototype.push;
+Router.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err);
+};
 
-export const constantRouterMap = [
-  ...base,
+export const constantRouterMap = [...base];
+
+export const asyncRouterMap = [
   map,
   electronicFence,
   warning,
   yawWarning,
   video,
-  journal
-
-]
-
-export const asyncRouterMap = [
+  journal,
   system,
   {
-    path: '*',
-    redirect: '/404',
+    path: "*",
+    redirect: "/404",
     hidden: true
   }
-]
+];
 export default new Router({
-  mode: 'history', // 后端支持可开
+  mode: "history", // 后端支持可开
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRouterMap
-})
+});

dbd703edea2a538c7e14adc14b574b2e7dbf0ee0
zj <chunbaook@163.com>
2020-12-30 11:54:48 +0800

    1230 视频监控区域调整

--- src/store/modules/gis.js
+++ src/store/modules/gis.js
@@ -10,19 +10,15 @@ const gisApp = {
   mutations: {
     addMap: (state, views) => {
       state.app.map = views
-      console.log(state.app)
     },
     saveCoordinate: (state, views) => {
       state.coordinate = views
-      console.log(state.coordinate)
     },
     setCoverId: (state, views) => {
       state.coverId = views
-      console.log(state.coverId)
     },
     setLonAndLat: (state, views) => {
       state.lonAndLat = views
-      console.log(state.lonAndLat)
     },
     setFactor: (state, type, factor) => {
       state.factor = factor
